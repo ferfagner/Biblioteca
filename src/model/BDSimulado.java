9362792697a54d0eb71dd0ad16bb817bda8d6863
ra21709716 <ra21709716@AN0800419077527.dinfor.uniceub>
2017-10-19 22:20:03 -0200

    oi

--- src/model/BDSimulado.java
+++ src/model/BDSimulado.java
@@ -1,24 +1,24 @@
 package model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class BDSimulado {
 	
-	private ArrayList<Livro> livro = new ArrayList<Livro>();
+	private ArrayList<Livro> livros = new ArrayList<Livro>();
 	private ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 	private ArrayList<Bibliotecario> bibliotecarios = new ArrayList<Bibliotecario>();
 	private ArrayList<Emprestimo> emprestimos = new ArrayList<Emprestimo>();
 	
 	public BDSimulado(){
 		//Criando Livros
-		Livro 11 = new Livro("Livro1","Autor1","Editora1","2012","123");
-		Livro 12 = new Livro("Livro1","Autor1","Editora1","2012","123");
-		Livro 13 = new Livro("Livro1","Autor1","Editora1","2012","123");
-		
+		Livro l1 = new Livro("Livro1", "Autor1", "Editora1", "2012", "123");
+		Livro l2 = new Livro("Livro1", "Autor1", "Editora1", "2012", "123");
+		Livro l3 = new Livro("Livro1", "Autor1", "Editora1", "2012", "123");
 		//Adicionando Livros
-		this.livros.add(11);
-		this.livros.add(12);
-		this.livros.add(13);
+		this.livros.add(l1);
+		this.livros.add(l2);
+		this.livros.add(l3);
 		
 		//Criando Usuarios
 		Usuario u1 = new Usuario("Usuario1", "123", "Curso1");
@@ -33,9 +33,19 @@ public class BDSimulado {
 		Bibliotecario b1 = new Bibliotecario("Bibli1", "123");
 		Bibliotecario b2 = new Bibliotecario("Bibli2", "456");
 		
+		//Adicionando Biblioteca
 		this.bibliotecarios.add(b1);
 		this.bibliotecarios.add(b2);
 		
+		//Criando emprestimo
+		Emprestimo e1= new Emprestimo(l1, u1, b2, LocalDate.of(2017, 10, 10), LocalDate.of(2017, 10, 15));
+		Emprestimo e2= new Emprestimo(l2, u2, b1, LocalDate.of(2017, 10, 15), LocalDate.of(2017, 10, 16));
+		Emprestimo e3= new Emprestimo(l3, u1, b1, LocalDate.of(2017, 10, 10), LocalDate.of(2017, 10, 20));
+		
+		//Adicionando Emprestimo
+		this.emprestimos.add(e1);
+		this.emprestimos.add(e2);
+		this.emprestimos.add(e3);
 	}
 
 
